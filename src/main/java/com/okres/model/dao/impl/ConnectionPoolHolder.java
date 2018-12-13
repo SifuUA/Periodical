13d66e75a63918bd19b452fb34a95e05b9cd4964
sifuua <alekseykres@gmail.com>
2018-12-13 16:27:03 +0200

    add registration form

--- src/main/java/com/okres/model/dao/impl/ConnectionPoolHolder.java
+++ src/main/java/com/okres/model/dao/impl/ConnectionPoolHolder.java
@@ -13,7 +13,7 @@ public class ConnectionPoolHolder {
             synchronized (ConnectionPoolHolder.class) {
                 if (dataSource == null) {
                     BasicDataSource ds = new BasicDataSource();
-                    ds.setUrl("jdbc:mysql://localhost:3306/mystudentdb");
+                    ds.setUrl("jdbc:mysql://localhost:3306/periodical");
                     ds.setUsername("phpmyadmin");
                     ds.setPassword("root");
                     ds.setMinIdle(5);
